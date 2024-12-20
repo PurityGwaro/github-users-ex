0219720af1410d2824734e5dc0f85326bbf2d539
Purity Gwaro <puritygwaro@gmail.com>
2024-12-20 22:11:58 +0300

    update card component

--- lib/github_users_web/components/card_component.ex
+++ lib/github_users_web/components/card_component.ex
@@ -38,11 +38,7 @@ defmodule GithubUsersWeb.CardComponent do
             value={@user_details["location"]}
             class="location"
           />
-          <.info_component
-            icon="hero-x-mark-solid"
-            value={@user_details["twitter_username"]}
-            class="twitter"
-          />
+          <.info_component svg_icon={true} value={@user_details["twitter_username"]} class="twitter" />
           <.info_component
             icon="hero-link-solid"
             value={@user_details["blog"]}
@@ -74,17 +70,34 @@ defmodule GithubUsersWeb.CardComponent do
   end
 
   attr :value, :any, required: true
-  attr :icon, :string, required: true
+  attr :icon, :string, required: false, default: nil
+  attr :svg_icon, :boolean, required: false, default: nil
   attr :class, :any, required: false, default: nil
 
   defp info_component(assigns) do
     ~H"""
-    <p class="flex items-start justify-start gap-4 w-full break-words overflow-hidden">
+    <p class={"flex items-start justify-start gap-4 w-full break-words overflow-hidden #{if @value in [nil, ""], do: " text-[#697C9A]", else: "dark:text-[#FFFFFF] text-[#4B6A9B]"} "}>
       <span>
-        <.icon name={@icon} class="w-[20px] h-[20px] bg-[#4B6A9B]" />
+        <%= if @icon do %>
+          <.icon
+            name={@icon}
+            class={"w-[20px] h-[20px] #{if @value in [nil, ""], do: "bg-[#4B6A9B]", else: "bg-black"}"}
+          />
+        <% else %>
+          <%= if @svg_icon do %>
+            <svg
+              height="18"
+              width="20"
+              xmlns="http://www.w3.org/2000/svg"
+              fill={"#{if @value in [nil, ""], do: "currentColor", else: "text-black"} "}
+            >
+              <path d="M20 2.799a8.549 8.549 0 01-2.363.647 4.077 4.077 0 001.804-2.266 8.194 8.194 0 01-2.6.993A4.099 4.099 0 009.75 4.977c0 .324.027.637.095.934-3.409-.166-6.425-1.8-8.452-4.288a4.128 4.128 0 00-.56 2.072c0 1.42.73 2.679 1.82 3.408A4.05 4.05 0 01.8 6.598v.045a4.119 4.119 0 003.285 4.028 4.092 4.092 0 01-1.075.135c-.263 0-.528-.015-.776-.07.531 1.624 2.038 2.818 3.831 2.857A8.239 8.239 0 01.981 15.34 7.68 7.68 0 010 15.285a11.543 11.543 0 006.29 1.84c7.545 0 11.67-6.25 11.67-11.667 0-.182-.006-.357-.015-.53A8.18 8.18 0 0020 2.798z" />
+            </svg>
+          <% end %>
+        <% end %>
       </span>
       <span class={[
-        "text-[#4B6A9B] dark:text-[#FFFFFF]",
+        "dark:text-[#FFFFFF]",
         @class
       ]}>
         <%= if @value in [nil, ""] do %>
